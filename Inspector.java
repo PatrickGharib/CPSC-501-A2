50ba5a5751a81622296a438073765b0b1612b001
PatrickGharib <patrick.a.gharib@gmail.com>
2019-03-06 15:48:30 -0700

    added initial methods

---
+++ Inspector.java
@@ -0,0 +1,21 @@
+import java.lang.reflect.*;
+public class Inspector{
+    public static void getDeclaringClass(){};
+    public static void getImmediateSuperClass(){};
+    public static void getImplementedInterfaces(){};
+    
+    public static void getDeclaredMethods(){};
+    public static void getMethodThrownExceptions(){};
+    public static void getMethodParameterTypes(){};
+    public static void getMethodReturnTypes(){};
+    public static void getMethodmodifiers(){};
+    
+    public static void getDeclaredConstructors(){};
+    public static void getConstructorParamaterTypes(){};
+    public static void getConstructorModifiers(){};
+
+    public static void getDeclaredFields(){};
+    public static void getFieldType(){};
+    public static void getFieldModifier(){};
+
+}
