d313a127b6d8e46954a7c6448fbdeb68ba170884
PatrickGharib <36083375+PatrickGharib@users.noreply.github.com>
2019-03-07 00:37:46 -0700

    REFACTOR: extract method
    Extract method: Inspector

--- Inspector.java
+++ Inspector.java
@@ -1,44 +1,73 @@
 import java.lang.reflect.*;
 public class Inspector{
-   public void inspect(Object object, Boolean recursive){
+   public void inspect(Object object, Boolean recursive) {
        Class classObject = object.getClass();
        Class superClassObject = classObject.getSuperclass();
        Class[] interfaceObjects = classObject.getInterfaces();
 
        Method methodObjects[] = classObject.getDeclaredMethods();
-       for (Method method : methodObjects){
-           Class methodExceptionTypes[] = method.getExceptionTypes();
-           Class methodParameterTypes[] = method.getParameterTypes();
-           Class methodReturnType = method.getReturnType();
-           int modifiers = method.getModifiers();
-           String toStringModifiers = Modifier.toString(modifiers);
-       }
+       methodInspector(methodObjects);
+
 
        Constructor constructorObjects[] = classObject.getConstructors();
-       for (Constructor constructor : constructorObjects){
+       for (Constructor constructor : constructorObjects) {
            Class constructorParameterTypes[] = constructor.getParameterTypes();
            int modifiers = constructor.getModifiers();
            String toStringModifiers = Modifier.toString(modifiers);
        }
 
        Field fieldObjects[] = classObject.getDeclaredFields();
-       for (Field field : fieldObjects){
+       for (Field field : fieldObjects) {
            Class fieldType = field.getType();
            int modifiers = field.getModifiers();
+           String toStringModifiers = Modifier.toString(modifiers);
 
        }
-
-
-
-
-
    }
-    //public static Class getDeclaringClass(Object objectToInspect){
-      //  Class classObject = objectToInspect.getClass();
-        //return classObject;
-        //System.out.println("Declaring Class: " + classObject);
+   /**
+    * inspect methods
+    * includes inspection of:
+    * exception types,parameter and return types,modifiers
+    * */
+       private void methodInspector(Method[] methodObjects){
+           for (Method method : methodObjects){
+               Class methodExceptionTypes[] = method.getExceptionTypes();
+               Class methodParameterTypes[] = method.getParameterTypes();
+               Class methodReturnType = method.getReturnType();
+               int modifiers = method.getModifiers();
+               String toStringModifiers = Modifier.toString(modifiers);
 
-   // };
+               System.out.println("method: " + toStringModifiers +
+                                   methodReturnType.getName() +
+                                   method.getName() +
+                                   "(" );
+                                   displayClassTypeObjects(methodParameterTypes);
+               System.out.println(")");
+
+               if (methodExceptionTypes.length > 0) {
+                   System.out.print(" THROWS ");
+                   displayClassTypeObjects(methodExceptionTypes);
+                   System.out.println();
+               }
+           }
+       }
+    //needs modification
+    private void displayClassTypeObjects(Class[] classTypeObjects){
+        if(classTypeObjects.length > 0){
+            for(Class c : classTypeObjects){
+                System.out.print(c.getName());
+
+                //add a comma if not last element in array
+                if(classTypeObjects[classTypeObjects.length -1] != c)
+                    System.out.print(", ");
+            }
+        }
+        else{System.out.print("");}
+    }
+
+
+}
+
     //public static void getImmediateSuperClass(){};
     // public static void getImplementedInterfaces(){};
     
@@ -60,4 +89,3 @@ public class Inspector{
         getDeclaringClass(object);
 
     }*/
-}
